300644b58aad327561fa1a7c1a9cf3268603f594
juniorjunco <juniorjunco@gmail.com>
2025-06-30 00:28:37 -0500

    okokokok

--- routes/bookings.js
+++ routes/bookings.js
@@ -18,9 +18,18 @@ const transporter = nodemailer.createTransport({
 router.post('/', async (req, res) => {
   try {
     const data = req.body;
+    // Construir dirección completa y mapsLink
+const fullAddress = `${data.street}, ${data.city}, ${data.country}, ${data.zip}`;
+const mapsLink = `https://www.google.com/maps?q=${encodeURIComponent(fullAddress)}&output=embed`;
+
 
     // Guardar reserva en DB
-    const newBooking = await Booking.create(data);
+    const newBooking = await Booking.create({
+  ...data,
+  address: fullAddress,
+  mapsLink: mapsLink
+});
+
 
     // Quitar la hora de la disponibilidad
     await Availability.updateOne(
